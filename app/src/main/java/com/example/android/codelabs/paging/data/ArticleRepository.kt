ea26235aaea45b7b341b5b59b260fa5f21b37c4b
Erbolbro <144798174+Erbolbro@users.noreply.github.com>
2024-03-10 13:21:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/ArticleRepository.kt
@@ -0,0 +1,11 @@
+
+package com.example.android.codelabs.paging.data
+
+import java.time.LocalDateTime
+
+private val firstArticleCreatedTime = LocalDateTime.now()
+
+class ArticleRepository {
+
+    fun articlePagingSource() = ArticlePagingSource()
+}
